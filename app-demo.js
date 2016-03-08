2b8afd8cc6dce4fb82e50f5748ff90936c2b38df
alope107 <kenkauffleman@gmail.com>
2016-03-07 17:48:45 -0800

    fancypants

--- app-demo.js
+++ app-demo.js
@@ -9,15 +9,20 @@ var Engine = Matter.Engine,
     Common = Matter.Common,
     Composites = Matter.Composites,
     MouseConstraint = Matter.MouseConstraint,
-	Events = Matter.Events;
+	Events = Matter.Events
+	Composite = Matter.Composite;
 
-var can_width = 195*2;
-var can_height = 325*2;
-var count = 55;
+var can_width = 317;
+var can_height = 522;
 
+var targ_dim = can_height / 15;
+var targ_x1 = can_width/2 - targ_dim;
+var targ_y1 = can_height/5 - targ_dim;
+var targ_x2 =  targ_x1 + 2 * targ_dim;
+var targ_y2 =  targ_y1 + 2 * targ_dim;
 
-//var can_height = 600;
-//var can_width = 800;
+var count = 0;
+
 // create a Matter.js engine
 var engine = Engine.create(document.body, {
   render: {
@@ -50,21 +55,22 @@ World.add(engine.world, [
     Bodies.rectangle(can_width/2, can_height + offset, can_width + .5 + 2 * offset, 50.5, options), //floor
     Bodies.rectangle(can_width + offset, can_height/2, 50.5, can_height + .5 + 2 * offset, options), //right
     Bodies.rectangle(-offset, can_height/2, 50.5, can_height + .5 + 2 * offset, options) //left
-    /*Bodies.rectangle(400, -offset, 800.5 + 2 * offset, 50.5, options),
-    Bodies.rectangle(400, 600 + offset, 800.5 + 2 * offset, 50.5, options),
-    Bodies.rectangle(800 + offset, 300, 50.5, 600.5 + 2 * offset, options),
-    Bodies.rectangle(-offset, 300, 50.5, 600.5 + 2 * offset, options)*/
 ]);
 
-var stack = Composites.stack(20, 20, 6, 3, 0, 0, function(x, y) {
+var radius = (can_width)/12 - 2;
+
+var stack = Composites.stack(20, can_height/2, 6, 3, 0, 0, function(x, y) {
     
-        return Bodies.circle(x, y, (can_width)/12 - 2, {
+        return Bodies.circle(x, y, radius, {
             density: 0.001,
             frictionAir: 0.006,
             restitution: 0.3,
             friction: 0.001
         });
 });
+
+var balls = stack.bodies;
+console.log(balls)
 
 World.add(engine.world, stack);
 
@@ -75,7 +81,17 @@ renderOptions.wireframes = false;
 
 Events.on(engine, 'tick', function(event) {
   countElement.textContent = count;
-  count +=1;
+  for (var i = 0; i < balls.length; i++) {
+	var ball = balls[i];
+	var x_pos = ball.position.x;
+	var y_pos = ball.position.y;
+	var thresh = .6 * can_height;
+	if(x_pos > targ_x1 && x_pos < targ_x2 && y_pos > targ_y1 && y_pos < targ_y2) {
+		var to_remove = ball.id;
+		Composite.remove(stack, ball);
+		count +=1;
+	}
+  }
   });
   
   
